1b88c9bffbd9d74396a9f7b9ea268602e2d94a91
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:38:54 +0800

    4117-45926-ALU-result1575173034641

--- src/ALU.java
+++ src/ALU.java
@@ -25,7 +25,8 @@ public class ALU {
 		int n = Integer.valueOf(tmpNum);
 		while (n >= 1) {
 			result.insert(0, String.valueOf(n % 2));
-			n = (n - n * 2) / 2;
+
+			n = (n - n % 2) / 2;
 		}
 		// 若是负数,取反加一
 		if (isMinus) {
@@ -91,7 +92,7 @@ public class ALU {
 		// 生成小数部分的二进制表示(若有)
 		StringBuilder afterDot = new StringBuilder();
 		if (strs.length > 1) {
-			float m = (float) (Integer.valueOf(strs[1])) * (float) Math.pow(10, strs[1].length());
+			float m = (float) (Integer.valueOf(strs[1])) * (float) Math.pow(10, -strs[1].length());
 			if (m == 0) {
 				afterDot = new StringBuilder(allZeroWithLength(eLength + sLength + 1));
 			} else {
@@ -113,7 +114,7 @@ public class ALU {
 		int bias = (int) Math.pow(2, eLength - 1) - 1;
 		if (beforeDot.toString().equals("")) {
 			e = normalize(afterDot.toString());
-			if (bias - e <= 0) {
+			if (bias - e < 0) {
 				// 反规格化
 				System.out.println(bias - e);
 				System.out.println(afterDot);
@@ -258,7 +259,7 @@ public class ALU {
 		while (tailNum.length() < dotPos) {
 			tailNum = tailNum + "0";
 		}
-		for (int i = dotPos - 1; i >= 0; i++) {
+		for (int i = dotPos - 1; i >= 0; i--) {
 			result += (float) (Math.pow(2, dotPos - 1 - i) * (tailNum.charAt(i) - 48));
 		}
 		for (int i = dotPos; i < sLength; i++) {
@@ -484,8 +485,9 @@ public class ALU {
 	// 返回长为n的全0串
 	private String allZeroWithLength(int n) {
 		StringBuilder result = new StringBuilder();
-
-		result.append("0");
+while (result.length() < n) {
+			result.append("0");
+		}
 
 		return result.toString();
 	}
